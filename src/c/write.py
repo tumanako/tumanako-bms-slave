44d5c23cfca02ce3b0145c8e98e9416cfed20f60
Tom Parker <tom@carrott.org>
2012-07-14 21:55:13 +0000

    write svn revision, programming date and whether there are local changes to eeprom
    git-svn-id: tumanako.svn.sourceforge.net::svn/tumanako/trunk@450 c3429617-7ab9-49b5-8233-5f90b2ce515f

--- src/c/write.py
+++ src/c/write.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python
 import os
 import sys
-
+import struct
+import pysvn
+import time
 from subprocess import Popen
 from subprocess import check_output
 from subprocess import check_call
@@ -37,31 +39,47 @@ def getBoolean(output, address):
 		return True
 	return None
 
-def booleanToString(b):
-	if (b):
-		return "01"
-	return "00"
+def getRevision():
+	client = pysvn.Client()
+	entry = client.info('.')
+	return entry.revision.number
+
+def getIsClean():
+	client = pysvn.Client()
+	statuses = client.status(".")
+	for status in statuses:
+		if status.is_versioned:
+			if status.text_status == pysvn.wc_status_kind.modified:
+				return False
+	return True
+	
+def makeEEDataHex(address, data):
+	# TODO use the address
+	h = ":%02x422000" % (len(data) * 2)
+	for b in data:
+		h = h + b.encode("hex") + "00"
+	h = h + "00\n"
+	h = h + ":00000001FF"
+	return h
 
 def writeData(cellId, kelvinConnection, resistorShunt):
-	cellIdHigh = hex((cellId & 0xff00) >> 8)[2:].zfill(2)
-	cellIdLow = hex(cellId & 0x00ff)[2:].zfill(2)
-	kelvinConnection = booleanToString(kelvinConnection)
-	resistorShunt = booleanToString(resistorShunt)
-	checksum = "00";
-
-	print "about to write ", cellIdLow, cellIdHigh, kelvinConnection, resistorShunt
+	revision = getRevision()
+	isClean = getIsClean()
+	print "about to write", cellId, kelvinConnection, resistorShunt, revision, isClean
+	
+	data = struct.pack("<HBBHBL", cellId, kelvinConnection, resistorShunt, revision, isClean, long(time.time()))
+	
 	eedata = open("eedata.hex", "w")
-	eedata.write(":08422000" + cellIdLow + "00" + cellIdHigh + "00" + kelvinConnection + "00" + resistorShunt + "00" + checksum + "\n")
-	eedata.write(":00000001FF")
+	eedata.write(makeEEDataHex(10, data))
 	eedata.close()
 	print check_output(["pk2cmd", "-PPIC16F688", "-M", "-Feedata.hex", "-R"])
 	
 	output = check_output(["pk2cmd", "-PPIC16F688", "-GE10-17", "-R"])
 	if cellId != getCellId(output):
 		raise ValueError("expected cell id " + cellId + " but got " + getCellId(output) + " " + output)
-	if kelvinConnection != booleanToString(getKelvinConnection(output)):
+	if kelvinConnection != getKelvinConnection(output):
 		raise ValueError("expected kelvin connection " + kelvinConnection + " but got " + str(getKelvinConnection(output)) + " " + output)
-	if resistorShunt != booleanToString(getResistorShunt(output)):
+	if resistorShunt != getResistorShunt(output):
 		raise ValueError("expected resistor shunt " + resistorShunt + " but got " + str(resistorShunt(output)) + " " + output)
 
 output = check_output(["pk2cmd", "-PPIC16F688", "-GE10-17", "-R"])
@@ -71,11 +89,8 @@ resistorShunt = getResistorShunt(output)
 
 print "found cellId:", cellId, "kelvin connection:", kelvinConnection, "resistorShunt", resistorShunt 
 
-updateEEData = False
-
 if (len(sys.argv) > 1):
 	if (sys.argv[1] != cellId):
-		updateEEData = True
 		cellId = int(sys.argv[1])
 
 if cellId == None:
@@ -83,15 +98,12 @@ if cellId == None:
 	
 if kelvinConnection == None:
 	kelvinConnection = False
-	updateEEData = True
 
 if resistorShunt == None:
 	resistorShunt = True
-	updateEEData = True
 
-if (updateEEData):	
-	print "writing cellId:", cellId, "kelvin connection:", kelvinConnection, "resistorShunt", resistorShunt 
-	writeData(cellId, kelvinConnection, resistorShunt)
+print "writing cellId:", cellId, "kelvin connection:", kelvinConnection, "resistorShunt", resistorShunt 
+writeData(cellId, kelvinConnection, resistorShunt)
 
 extra = "-DCELL_ID_LOW=" + str(cellId & 0xff) + " -DCELL_ID_HIGH=" + str(cellId >> 8)
 if resistorShunt:
